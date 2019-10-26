de850b02adceb57cb6120bede61d1e29c4869168
andreas <Andreas.Glaser@hof-university.de>
2019-10-26 22:45:08 +0200

    ranking added (css werden nicht geladen)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatchComponent } from './match/match.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { RouterModule, Routes } from '@angular/router';
+import {ChartsModule} from 'ng2-charts';
+import {MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatTableModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+const appRoutes: Routes = [
+  { path: 'match', component: MatchComponent },
+  { path: 'ranking', component: RankingComponent }
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchComponent,
+    RankingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes),
+    ChartsModule,
+    MatCheckboxModule,
+    MatTableModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
